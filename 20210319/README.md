e9d2790c37134586ae50fae11cf6a1a2c0fdeb32
github shiory602 <suzushioufo@gmail.com>
2021-03-23 23:03:23 -0700

    add edit functions

--- 20210319/README.md
+++ 20210319/README.md
@@ -2,3 +2,8 @@
 1. 前回同様リストを作る
 2. 名前をクリックすると編集用小窓が開くようにする
 3. 年齢も同様
+
+ヒント
+- event.currenttarget.valueを使う
+https://www.w3schools.com/jsref/event_currenttarget.asp
+- li内の要素はそれぞれ作る
---
+++ 20210319/js/main.js
@@ -0,0 +1,72 @@
+let names = document.getElementById("name");
+let age = document.getElementById("age");
+let ascBtn = document.getElementById("ascBtn");
+let descBtn = document.getElementById("descBtn");
+let listItem = document.getElementById("list-item");
+
+let ul = document.createElement("ul");
+listItem.appendChild(ul);
+let arr = new Array();
+
+addList.addEventListener("submit", (e) => {
+	e.preventDefault();
+	if (!names.value || !age.value) {
+		alert("Please fill out both forms :)");
+		return;
+	}
+	let li = document.createElement("li");
+	li.innerHTML = `<p id="nameItem" onclick="nameFunction('${names.value}')">${names.value}</p>(<span id="ageItem" onclick="ageFunction('${age.value}')">${age.value}</span>)`;
+	console.log(li);
+	ul.appendChild(li);
+
+	let obj = {
+		key: age.value,
+		value: li,
+	}
+	arr.push(obj);
+
+	names.value = "";
+	age.value = "";
+})
+
+
+// Add event ///////////////////////////////////////////
+ascBtn.addEventListener("click", () => {
+	arr.sort(sortAsc);
+	for (let i = 0; i < arr.length; i++) {
+		ul.appendChild(arr[i].value);
+	}
+})
+////////////////////////////////////////////////////////
+
+// Sort event //////////////////////////////////////////
+let sortAsc = (a, b) => {
+	return a.key.localeCompare(b.key);
+}
+
+descBtn.addEventListener("click", () => {
+	arr.sort(sortDesc);
+	for (let i = 0; i < arr.length; i++) {
+		ul.appendChild(arr[i].value);
+	}
+})
+
+let sortDesc = (a, b) => {
+	return b.key.localeCompare(a.key);
+}
+////////////////////////////////////////////////////////
+
+// edit event /////////////////////////////////////////
+function nameFunction(e) {
+	let personName = prompt("You can edit the name here!", e);
+	if (personName != null) {
+    document.getElementById("nameItem").innerText = personName;
+  }
+}
+
+function ageFunction(e) {
+	let personAge = prompt("You can edit the age here!", e);
+	if (personAge != null) {
+    document.getElementById("ageItem").innerText = personAge;
+  }
+}
