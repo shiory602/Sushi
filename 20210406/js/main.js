faf3c66b0442c3e5a47e90580fdd1165ddb94d13
github shiory602 <suzushioufo@gmail.com>
2021-04-07 20:59:57 -0700

    organized code add code for JSON

--- 20210406/js/main.js
+++ 20210406/js/main.js
@@ -32,47 +32,64 @@ addBtn.addEventListener('submit', e => {
 });
 
 ////////////////////////////////////////////////////
-    // Sort data belong to the order
-    ascBtn.addEventListener('click', () => {
-        arr.sort(sortAsc);
-        // for(let item of arr) {
-        //     ul.appendChild(item.value);
-        // }
-        arr.map(item => ul.appendChild(item.value));
-    });
-    const sortAsc = (a, b) => {
-        return a.key - b.key;
-    }
+// Sort data belong to the order
+ascBtn.addEventListener('click', () => {
+    arr.sort(sortAsc);
+    // for(let item of arr) {
+    //     ul.appendChild(item.value);
+    // }
+    arr.map(item => ul.appendChild(item.value));
+});
+const sortAsc = (a, b) => {
+    return a.key - b.key;
+}
 
-    dscBtn.addEventListener('click', () => {
-        arr.sort(sortDsc);
-        arr.map(item => ul.appendChild(item.value));
-        // for (let i = 0; i < arr.length; i++) {
-        //     ul.appendChild(arr[i].value);
-        // }
-    });
-    const sortDsc = (a, b) => {
-        return b.key - a.key;
-    }
+dscBtn.addEventListener('click', () => {
+    arr.sort(sortDsc);
+    arr.map(item => ul.appendChild(item.value));
+    // for (let i = 0; i < arr.length; i++) {
+    //     ul.appendChild(arr[i].value);
+    // }
+});
+const sortDsc = (a, b) => {
+    return b.key - a.key;
+}
 ////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////
-    // Change data function
-    const editName = e => {
-        const elName = e.currentTarget;
-        const nameValue = prompt('Do you want to change your name?', elName.textContent);
-        if(!nameValue) {
-            return;
-        }
-        elName.innerHTML = nameValue;
+// Change data function
+const editName = e => {
+    const elName = e.currentTarget;
+    const nameValue = prompt('Do you want to change your name?', elName.textContent);
+    if (!nameValue) {
+        return;
     }
+    elName.innerHTML = nameValue;
+}
 
-    const editAge = e => {
-        const elAge = e.currentTarget;
-        const ageValue = prompt('Do you want to change your age?', elAge.textContent);
-        if(!ageValue || typeof(ageValue) !== 'number' || ageValue < 0) {
-            return;
-        }
-        elAge.innerHTML = ageValue;
+const editAge = e => {
+    const elAge = e.currentTarget;
+    const ageValue = prompt('Do you want to change your age?', elAge.textContent);
+    if (!ageValue || typeof (ageValue) !== 'number' || ageValue < 0) {
+        return;
     }
+    elAge.innerHTML = ageValue;
+}
 //////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+
+KEY = 'super-cool-data';
+
+// 保存
+data = [{
+    name: "Alice",
+    age: 12
+}];
+
+json = JSON.stringify(data);
+window.localStorage.setItem(KEY, json);
+
+// 取得
+json = window.localStorage.getItem(KEY);
+data = JSON.parse(json);
