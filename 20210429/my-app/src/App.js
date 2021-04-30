de414c20a513c047a018070c338d6e96d3d612d6
shiory602 <suzushioufo@gmail.com>
2021-04-29 23:28:05 -0700

    Hello to React World Co-authored-by: Ginpei

--- 20210429/my-app/src/App.js
+++ 20210429/my-app/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const message = "Hello World!";
+
+  const movies = [
+    {
+      id: "123",
+      title: "となりのトトロ",
+    },
+    {
+      id: "234",
+      title: "もののけ姫",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NiceHeading>{message}</NiceHeading>
+      <NewMovieForm />
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>映画名：{movie.title}</li>
+        ))}
+      </ul>
     </div>
+  );
+}
+
+function NiceHeading({ children }) {
+  return (
+    <h1 style={{ color: "tomato" }}>{children}</h1>
+  );
+}
+
+function NewMovieForm() {
+  const [movie, setMovie] = useState({
+    title: "",
+    rate: 5,
+  });
+
+  const onValueChange = (event) => {
+    const { name, value } = event.currentTarget;
+    if (name === "title") {
+      setMovie({ ...movie, title: value });
+    } else if (name === "rate") {
+      setMovie({ ...movie, rate: Number(value) });
+    }
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(movie);
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <p>
+        <label>
+          題名：
+          <input type="text" name="title" value={movie.title} onChange={onValueChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          評価：
+          <input type="number" name="rate" value={movie.rate} onChange={onValueChange} />
+        </label>
+      </p>
+      <p>
+        <button>作成</button>
+      </p>
+    </form>
   );
 }
 
