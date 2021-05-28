157c64b5f5aa03bf308696b6cf891d93ddfe8fb9
shiory602 <suzushioufo@gmail.com>
2021-05-27 22:47:03 -0700

    solution

--- 20210527reactclickbtn/src/App.js
+++ 20210527reactclickbtn/src/App.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(()=> {
+    if(count < 0) setCount(0);
+  }, [count])
+  
+  function addCount() {
+    setCount(count + 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
   return (
     <div className="App">
-      hello world
+      <button onClick={ addCount }>+1</button>
+      <button onClick={ resetCount }>Reset</button>
+      <h1>Count: { count }</h1>
     </div>
   );
 }
