6775643a9008344b05f80cd26aa9767983614c4e
shiory602 <suzushioufo@gmail.com>
2021-05-29 13:16:00 -0700

    clear the data

---
+++ 20210527reactclickbtn/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Functioncpt from "./Components/Functioncpt";
+import Classcpt from "./Components/Classcpt";
+
+function App() {
+  return (
+    <div className="App">
+      <Functioncpt />
+      <hr />
+      <Classcpt />
+    </div>
+  );
+}
+
+export default App;
