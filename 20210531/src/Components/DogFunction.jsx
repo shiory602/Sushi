0b27bad11b0ec26b85e04d91d0b74401a637ec9f
shiory602 <suzushioufo@gmail.com>
2021-06-11 19:27:32 -0700

    fix property name and error

--- 20210531/src/Components/DogFunction.jsx
+++ 20210531/src/Components/DogFunction.jsx
@@ -10,30 +10,37 @@ export default function DogFunction () {
     useEffect(() => {
         // loading状態開始
         setLoading(true);
-        const url1 = fetchDogImageUrl();
-        const url2 = fetchDogImageUrl();
-        const url3 = fetchDogImageUrl();
+        const dog1 = fetchDogImageUrl();
+        const dog2 = fetchDogImageUrl();
+        const dog3 = fetchDogImageUrl();
 
 
         // 非同期処理 Promise.allを使って３つのpromiseを同時に実行
-        Promise.all([url1, url2, url3])
-            .then(([url1, url2, url3]) => {
-                setDogs1(url1);
-                setDogs2(url2);
-                setDogs3(url3);
+        Promise.all([dog1, dog2, dog3])
+            .then(([dog1, dog2, dog3]) => {
+                setDogs1(dog1);
+                setDogs2(dog2);
+                setDogs3(dog3);
             })
             // いずれかの非同期処理が失敗
-            .catch((error) => console.error('エラー', error))
+            .catch((response) => {
+                if (!response.ok) {
+                    console.response("エラーレスポンス", response);
+                } else {
+                    // Responseオブジェクト の jsonメソッドも Promise を返す
+                    // HTTPレスポンスボディ を JSON としてパースしたオブジェクトでresolveされる
+                    return response.json();
+                }
+            })
             // pが完了したらloading状態終了　finallyはthenとcatchの両方
             .finally(() => setLoading(false));
     }, []);
     return(
         <>
-                {/* {console.log(dogs1.url)} */}
             <h1>Function Hooks</h1>
-            {loading ? <img src={dogs1.url1} className='dog' alt='wan-wan1' /> : <p>loading...</p>}
-            {loading ? <img src={dogs2.url} className='dog' alt='wan-wan2' /> : <p>loading...</p>}
-            {loading ? <img src={dogs3.url} className='dog' alt='wan-wan3' /> : <p>loading...</p>}
+            {loading ? <p>loading...</p> : <img src={dogs1.message} className='dog' alt='wan-wan1' />}
+            {loading ? <p>loading...</p> : <img src={dogs2.message} className='dog' alt='wan-wan2' />}
+            {loading ? <p>loading...</p> : <img src={dogs3.message} className='dog' alt='wan-wan3' />}
         </>
     )
 }
@@ -46,12 +53,8 @@ function fetchDogImageUrl() {
     return fetch('https://dog.ceo/api/breeds/image/random')
     // Promiseインスタンス は Responseオブジェクトで resolve され、 thenコールバックが呼び出される
     .then(response => {
-        if (!response.ok) {
-            console.error("エラーレスポンス", response);
-        } else {
-            // Responseオブジェクト の jsonメソッドも Promise を返す
-            // HTTPレスポンスボディ を JSON としてパースしたオブジェクトでresolveされる
-            return response.json()
-        }
+        // Responseオブジェクト の jsonメソッドも Promise を返す
+        // HTTPレスポンスボディ を JSON としてパースしたオブジェクトでresolveされる
+        return response.json();
     })
 }
